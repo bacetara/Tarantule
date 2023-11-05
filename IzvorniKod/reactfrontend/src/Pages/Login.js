20c47bd453e6d588b0c32198cd6d4a2293b47083
miacrnolatac <mc54018@fer.hr>
2023-11-05 18:33:07 +0100

    login stranica

---
+++ IzvorniKod/reactfrontend/src/Pages/Login.js
@@ -0,0 +1,19 @@
+const Login = ()  => {
+    return (
+        <div className="container">
+            <div className="text">OIB
+            </div>
+            <div>
+                <input className="oib" type="text" />
+            </div>
+            <div className="text">lozinka</div>
+            <div>
+                <input className="lozinka" type="text" />
+            </div>
+            <button className="loginbutton" >
+                prijava/registracija
+            </button>
+        </div>
+    );
+};
+export default Login;
