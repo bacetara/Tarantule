c4fda709fa6b92acd8e59baadcabef2322bb3896
Thyncha <jerbicalan@gmail.com>
2023-11-10 22:36:49 +0100

    Update baza.sql
    Dodao testne primjere

--- IzvorniKod/bazapodataka/baza.sql
+++ IzvorniKod/bazapodataka/baza.sql
@@ -29,7 +29,7 @@ CREATE TABLE Osoba
   Prezime VARCHAR NOT NULL,
   mail VARCHAR,
   datumRod DATE,
-  adresa VARCHAR NOT NULL,
+  adresa VARCHAR,
   adminPrava INT NOT NULL,
   lozinka VARCHAR,
   Uloga VARCHAR NOT NULL,
@@ -54,4 +54,9 @@ CREATE TABLE Poruka
   FOREIGN KEY (priOIB) REFERENCES Osoba(OIB),
   FOREIGN KEY (pošOIB) REFERENCES Osoba(OIB),
   FOREIGN KEY (dijagnozaID) REFERENCES Bolest(idBolest)
-);
+);--data import
+INSERT INTO Osoba (OIB,Ime,Prezime,mail,datumRod,adresa,adminPrava,lozinka,Uloga,rodOIB,dokOIB) values (101,'Iva','Ivić',NULL,'2001-01-01',NULL,0,'lozinka','doktor',NULL,NULL);
+INSERT INTO Osoba (OIB,Ime,Prezime,mail,datumRod,adresa,adminPrava,lozinka,Uloga,rodOIB,dokOIB) values (100,'Ivan','Ivanić',NULL,'2002-10-01',NULL,0,'password','roditelj',NULL,101);
+INSERT INTO Osoba (OIB,Ime,Prezime,mail,datumRod,adresa,adminPrava,lozinka,Uloga,rodOIB,dokOIB) values (102,'Roko','Luk',NULL,'2020-10-10',NULL,0,'test','dijete',100,NULL);
+
+
