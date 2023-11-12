e14da3616d8595d0403a04d491a64c526e1f7f18
miacrnolatac <mc54018@fer.hr>
2023-11-12 17:04:35 +0100

    dodavanje name-ova

--- IzvorniKod/reactfrontend/src/Pages/Admin/DodajDijete.js
+++ IzvorniKod/reactfrontend/src/Pages/Admin/DodajDijete.js
@@ -1,16 +1,39 @@
 import './DodajRoditelja.css'
 import {Link} from "react-router-dom";
 
+
+/*function onSubmit(e){
+    e.preventDefault();
+    const data ={
+        firstname: ,
+        lastname: ,
+        oib: ,
+        oibParent:,
+        birthdate:
+    };
+
+    const options = {
+        method: POST;
+        headers:{
+
+        }
+        body:
+    }
+}*/
+
+
+
 const DodajDijete = () => {
     return (
         <div className="containerdodajroditelja">
+            <form >
             <div>NOVO DIJETE</div>
-            <div className="info"><label>IME: </label><input type="text"   /></div>
-            <div className="info"><label>PREZIME: </label><input type="text"   /></div>
-            <div className="info"><label>OIB: </label><input type="text"   /></div>
-            <div className="info"><label>OIB RODITELJA: </label><input type="text"   /></div>
-            <div className="info"><label>DATUM RODJENJA: </label><input type="text"   /></div>
-
+            <div className="info"><label>IME: </label><input name="firstname" type="text"   /></div>
+            <div className="info"><label>PREZIME: </label><input name="lastname" type="text"   /></div>
+            <div className="info"><label>OIB: </label><input name="oib" type="text"   /></div>
+            <div className="info"><label>OIB RODITELJA: </label><input name="oibparent" type="text"   /></div>
+            <div className="info"><label>DATUM RODJENJA: </label><input name="birthdate" type="date"   /></div>
+        </form>
             <div className="addPerson"> <Link className="link_na_stranicu" to="/admin">Dodaj osobu</Link> </div>
 
 
