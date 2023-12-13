41d0321ab579fa35880613572b41ceb4fd09ed2a
Josip <josip.pavlic@fer.hr>
2023-12-14 00:25:34 +0100

    adding parent children info site

---
+++ IzvorniKod/reactfrontend/src/Pages/Parent/RoditeljPocetna.js
@@ -0,0 +1,17 @@
+import './RoditeljPocetna.css';
+
+export default function RoditeljPocetna() {
+	return (
+		<div className="infocontainer">
+			<div>
+				<p>ime i prezime</p>
+			</div>
+			<div>
+				<p>dijete</p>
+			</div>
+			<div>
+				<p>nesto trece</p>
+			</div>
+		</div>
+	);
+}
