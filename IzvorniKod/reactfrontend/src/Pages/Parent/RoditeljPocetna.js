64c316179899cb8a0596d2252cfdcfec83b9c78b
josippavlic12 <149066205+josippavlic12@users.noreply.github.com>
2024-01-08 16:13:49 +0100

    updateing

--- IzvorniKod/reactfrontend/src/Pages/Parent/RoditeljPocetna.js
+++ IzvorniKod/reactfrontend/src/Pages/Parent/RoditeljPocetna.js
@@ -1,16 +1,40 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import './RoditeljPocetna.css';
+import json from './../../testing.json';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import * as React from "react";
+
 export default function RoditeljPocetna() {
+	useEffect(() => {
+		console.log(json);
+	}, []); // Empty dependency array means this effect runs once when the component mounts
+
 	return (
-		<div className="infocontainer">
-			<div>
-				<p>ime i prezime</p>
+		<>
+			<div className="header">
+				<div className="backOptions">
+					<div className="logOut">
+						<FontAwesomeIcon id="logOutIcon" icon={faArrowRightFromBracket} style={{ color: "white" }} />
+						<p id="logOutText">log out</p>
+					</div>
+				</div>
+
+				<div className="profileName">Roditelj [904238734]</div>
 			</div>
-			<div>
-				<p>dijete</p>
+			<div className="infocontainer">
+				{json.map(record => (
+					record.rodOIB === "01020304050" && (
+						<div className="child" key={record.OIB}>
+							{/* Use the Link component for routing */}
+							<Link className="link_na_stranicu" to={`/inbox/${record.OIB}`}>
+								{`${record.Ime} ${record.Prezime}`}
+							</Link>
+						</div>
+					)
+				))}
 			</div>
-			<div>
-				<p>nesto trece</p>
-			</div>
-		</div>
+		</>
 	);
 }
