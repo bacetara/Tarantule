792cf9de5a924b7198adf4a1e79729486f56f47b
ebosnjak <eugen.bosnjak@fer.hr>
2024-01-09 21:34:12 +0100

    Added exception message

--- IzvorniKod/springbackend/src/main/java/hr/fer/progi/tarantule/OzdraviBE/service/NoSuchOsobaException.java
+++ IzvorniKod/springbackend/src/main/java/hr/fer/progi/tarantule/OzdraviBE/service/NoSuchOsobaException.java
@@ -8,4 +8,12 @@ import java.io.Serial;
 public class NoSuchOsobaException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 10L;
+
+    public NoSuchOsobaException() {
+
+    }
+
+    public NoSuchOsobaException(String message) {
+        super(message);
+    }
 }
