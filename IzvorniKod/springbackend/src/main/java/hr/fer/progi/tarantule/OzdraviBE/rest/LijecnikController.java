c0be869cc7dad5b8cd85b8b6c30153f1fb66ac01
ebosnjak <eugen.bosnjak@fer.hr>
2024-01-13 23:18:40 +0100

    Completed doctor rest controller

--- IzvorniKod/springbackend/src/main/java/hr/fer/progi/tarantule/OzdraviBE/rest/LijecnikController.java
+++ IzvorniKod/springbackend/src/main/java/hr/fer/progi/tarantule/OzdraviBE/rest/LijecnikController.java
@@ -1,12 +1,21 @@
 package hr.fer.progi.tarantule.OzdraviBE.rest;
 
+import hr.fer.progi.tarantule.OzdraviBE.domain.Osoba;
 import hr.fer.progi.tarantule.OzdraviBE.domain.Poruka;
 import hr.fer.progi.tarantule.OzdraviBE.rest.dto.AddBolovanjeMessageDTO;
 import hr.fer.progi.tarantule.OzdraviBE.rest.dto.AddMessageDTO;
+import hr.fer.progi.tarantule.OzdraviBE.service.OsobaService;
 import hr.fer.progi.tarantule.OzdraviBE.service.PorukaService;
+import hr.fer.progi.tarantule.OzdraviBE.service.exceptions.InvalidAuthorizationException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/doctor/")
 public class LijecnikController {
@@ -14,8 +23,12 @@ public class LijecnikController {
     @Autowired
     private PorukaService porukaService;
 
+    @Autowired
+    private OsobaService osobaService;
+
+    @Secured("doktor")
     @PutMapping(path = "newMessage", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addMessage(@RequestBody AddMessageDTO messageData) {
+    public void addMessage(@RequestBody AddMessageDTO messageData, HttpServletRequest request, HttpServletResponse response) {
         Poruka p = new Poruka();
         p.setNaslov(messageData.naslov());
         p.setTijelo(messageData.tijelo());
@@ -29,8 +42,26 @@ public class LijecnikController {
         porukaService.createPoruka(p);
     }
 
+    @Secured("doktor")
+    @GetMapping("me")
+    public List<Osoba> getPatients(HttpServletRequest request, HttpServletResponse response) {
+        Osoba o = SecurityHelper.getAuthenticatedOsoba(request);
+        if (o == null) {
+            throw new InvalidAuthorizationException();
+        }
+
+        return osobaService.findByDoctor(o.getOib());
+    }
+
+    @Secured("doktor")
+    @GetMapping("unassigned")
+    public List<Osoba> getUnassignedPatients(HttpServletRequest request, HttpServletResponse response) {
+        return osobaService.findUnassigned("roditelj");
+    }
+
+    @Secured("doktor")
     @PutMapping(path = "enable", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void enable(@RequestBody AddBolovanjeMessageDTO messageData) {
+    public void enable(@RequestBody AddBolovanjeMessageDTO messageData, HttpServletRequest request, HttpServletResponse response) {
         Poruka p = new Poruka();
         p.setNaslov("Bolovanje");
         p.setTijelo("Dopušteno bolovanje i ovaj mail je službena potvrda.");
