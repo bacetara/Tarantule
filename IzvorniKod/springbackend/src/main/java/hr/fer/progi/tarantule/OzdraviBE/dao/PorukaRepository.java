fe10b23ab18581ba7313549a8a76e5f219f87fea
ebosnjak <eugen.bosnjak@fer.hr>
2024-01-18 23:10:14 +0100

    All message queries ordered by id descending so newer messages are shown on top

--- IzvorniKod/springbackend/src/main/java/hr/fer/progi/tarantule/OzdraviBE/dao/PorukaRepository.java
+++ IzvorniKod/springbackend/src/main/java/hr/fer/progi/tarantule/OzdraviBE/dao/PorukaRepository.java
@@ -9,21 +9,21 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PorukaRepository extends JpaRepository<Poruka, Integer> {
-    @Query("select p from Poruka p where p.tip = :type")
+    @Query("select p from Poruka p where p.tip = :type order by p.id desc")
     List<Poruka> findByType(@Param("type") String type);
 
-    @Query("select p from Poruka p where p.posoib = :oib")
+    @Query("select p from Poruka p where p.posoib = :oib order by p.id desc")
     List<Poruka> findBySender(@Param("oib") String senderOIB);
 
-    @Query("select p from Poruka p where p.prioib = :oib")
+    @Query("select p from Poruka p where p.prioib = :oib order by p.id desc")
     List<Poruka> findByRecipient(@Param("oib") String recipientOIB);
 
-    @Query("select p from Poruka p where p.prioib  = :oib or p.posoib  = :oib")
+    @Query("select p from Poruka p where p.prioib  = :oib or p.posoib  = :oib order by p.id desc")
     List<Poruka> findByOib(@Param("oib") String recipientOIB);
 
-    @Query("select p from Poruka p where (p.prioib = :oib1 and p.posoib = :oib2) or (p.prioib = :oib2 and p.posoib = :oib1)")
+    @Query("select p from Poruka p where (p.prioib = :oib1 and p.posoib = :oib2) or (p.prioib = :oib2 and p.posoib = :oib1) order by p.id desc")
     List<Poruka> findBetweenPersons(@Param("oib1") String oib1, @Param("oib2") String oib2);
 
-    @Query("select p from Poruka p join Osoba o on p.posoib = o.oib where p.prioib = :oib and (o.uloga = 'pedijatar' or o.uloga = 'doktor')")
+    @Query("select p from Poruka p join Osoba o on p.posoib = o.oib where p.prioib = :oib and (o.uloga = 'pedijatar' or o.uloga = 'doktor') order by p.id desc")
     List<Poruka> findReceivedFromDoctors(@Param("oib") String oib);
 }
